51eb9cfa39e7fc7324284c4549b1b84440097e99
Martin Siklosi <martin@siklosi.se>
2024-08-06 23:17:30 +0200

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn parse_character(character: char) -> Option<Instruction> {
     }
 }
 
-fn match_bracket_locations(bytecode: &Bytecode) -> Bytecode {
+fn match_brackets(bytecode: &Bytecode) -> Bytecode {
     let mut result = bytecode.clone();
     let mut open_locations = Vec::new();
     for (i, instruction) in bytecode.iter().enumerate() {
@@ -55,35 +55,25 @@ fn match_bracket_locations(bytecode: &Bytecode) -> Bytecode {
 }
 
 fn compile(source_code: String) -> Bytecode {
-    let bytecode: Vec<Instruction> = source_code
-        .chars()
-        .filter_map(|character| parse_character(character))
-        .collect();
-    match_bracket_locations(&bytecode)
+    let bytecode: Vec<Instruction> = source_code.chars().filter_map(parse_character).collect();
+    match_brackets(&bytecode)
 }
 
-fn print_byte(byte: &u8) -> () {
-    let utf8_array = [byte.to_owned(); 1];
-    let s = str::from_utf8(&utf8_array).expect("Byte should be printable");
-    print!("{}", s);
-}
-
-fn execute(bytecode: &Bytecode) -> () {
+fn execute(bytecode: &Bytecode) {
     const MEMORY_SIZE: usize = 30_000;
     let mut memory = [0u8; MEMORY_SIZE];
-    let mut data_pointer: usize = 0;
-    let mut instruction_pointer: usize = 0;
+    let mut data_pointer = 0;
+    let mut instruction_pointer = 0;
     while instruction_pointer < bytecode.len() && data_pointer < MEMORY_SIZE {
         match bytecode[instruction_pointer] {
             Instruction::IncPointer => data_pointer += 1,
             Instruction::DecPointer => data_pointer -= 1,
             Instruction::IncByte => memory[data_pointer] += 1,
             Instruction::DecByte => memory[data_pointer] -= 1,
-            Instruction::Output => print_byte(&memory[data_pointer]),
+            Instruction::Output => print!("{}", memory[data_pointer] as char),
             Instruction::Input => {
                 let input: String = read!("{}\n");
-                let input_byte = input.as_bytes()[0];
-                memory[data_pointer] = input_byte;
+                memory[data_pointer] = input.bytes().next().unwrap();
             }
             Instruction::OpenBracket {
                 matching_location: jump_location,
@@ -109,8 +99,8 @@ fn execute(bytecode: &Bytecode) -> () {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let path = args[1].to_owned();
-    let hello_world = fs::read_to_string(path).expect("Path should be valid");
-    let bytecode = compile(hello_world);
+    let path = &args[1];
+    let source_code = fs::read_to_string(path).expect("Path should be valid");
+    let bytecode = compile(source_code);
     execute(&bytecode);
 }
